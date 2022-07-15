e83b4183d7d2136f430ef2500dac9d694c7a36cf
Batuhan Ayyıldız <84630121+Bagi12@users.noreply.github.com>
2022-07-15 23:33:38 +0300

    Initial Commit

---
+++ LibraryAutomation/src/main/java/com/example/LibraryAutomation/LibraryAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAutomationApplication.class, args);
+	}
+
+}
---
+++ LibraryAutomation/src/test/java/com/example/LibraryAutomation/LibraryAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
