8245cefa57701c953b0665b5908ba12fa1d1c6a3
unknown <bloodmoney852@hotmail.com>
2022-07-29 20:42:25 +0300

    Category Serv Impl'daki mapper hatası giderildi. Security dependency'leri ayarlandı

--- library-automation-application/src/main/java/com/todeb/batuhanayyildiz/libraryautomationapplication/model/dto/CategoryDTO.java
+++ library-automation-application/src/main/java/com/todeb/batuhanayyildiz/libraryautomationapplication/model/dto/CategoryDTO.java
@@ -1,8 +1,13 @@
 package com.todeb.batuhanayyildiz.libraryautomationapplication.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class CategoryDTO {
     private String categoryName;
 }
--- library-automation-application/src/main/java/com/todeb/batuhanayyildiz/libraryautomationapplication/model/entity/Category.java
+++ library-automation-application/src/main/java/com/todeb/batuhanayyildiz/libraryautomationapplication/model/entity/Category.java
@@ -8,8 +8,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 
 
 @Entity
